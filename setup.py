efab5b0096023e82b7d7ba4b776cb63c6687cc13
Ayoub-Sakhi <167138841+Ayoub-Sakhi@users.noreply.github.com>
2024-05-17 16:46:53 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='invoice_calculator',
+    name='CALC_AYOUB',
     version='0.1',
     packages=find_packages(),
     description='A simple invoice calculator',
